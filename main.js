9e295aa48290e544ecef907f63916ab42a50d98a
Joseph Loper <jd.loper@icloud.com>
2024-05-06 14:09:34 -0700

    Add basic arithmetic functions

---
+++ main.js
@@ -0,0 +1,19 @@
+let operand1;
+let operator;
+let operand2;
+
+function add(operand1, operand2) {
+    return operand1 + operand2;
+}
+
+function subtract(operand1, operand2) {
+    return operand1 - operand2;
+}
+
+function multiply(operand1, operand2) {
+    return operand1 * operand2;
+}
+
+function divide(operand1, operand2) {
+    return operand1 / operand2;
+}
