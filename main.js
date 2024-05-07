ccc88854ce04f750a41049a0609a701c78c4ac02
Joseph Loper <jd.loper@icloud.com>
2024-05-06 18:24:55 -0700

    Add switch cases for operators

--- main.js
+++ main.js
@@ -17,3 +17,16 @@ function multiply(operand1, operand2) {
 function divide(operand1, operand2) {
     return operand1 / operand2;
 }
+
+function operate(operator, operand1, operand2) {
+    switch (operator) {
+        case '+':
+            return add(operand1, operand2)
+        case '-':
+            return subtract(operand1, operand2)
+        case '*':
+            return multiply(operand1, operand2)
+        case '/':
+            return divide(operand1, operand2)
+    }
+}
